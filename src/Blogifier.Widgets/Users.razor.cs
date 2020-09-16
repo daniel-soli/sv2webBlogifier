6444bc3e483c309ef385c6ae6aa08cf84467375c
Daniel Soli Stensvik <daniel.soli.stensvik@gmail.com>
2020-09-16 13:44:29 +0200

    Added Users in navmenu, users page and users component

---
+++ src/Blogifier.Widgets/Users.razor.cs
@@ -0,0 +1,14 @@
+﻿using Blogifier.Core.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blogifier.Widgets
+{
+    public partial class Users
+    {
+        [Inject]
+        protected IDataService DataService { get; set; }
+    }
+}
